301e6f087aae7cda9d186bc3eb9ad5f957553a8d
Isaac Hayward <Kodingroo@gmail.com>
2019-03-04 16:28:12 +0900

    user_document and document models added and validated

--- db/migrate/20190304065206_create_user_documents.rb
+++ db/migrate/20190304065206_create_user_documents.rb
@@ -2,7 +2,7 @@ class CreateUserDocuments < ActiveRecord::Migration[5.2]
   def change
     create_table :user_documents do |t|
       t.references :user, foreign_key: true
-      t.referecnes :document
+      t.references :document
       t.string :title
       t.string :type
       t.string :photo
