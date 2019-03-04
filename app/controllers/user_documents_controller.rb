319c54710955888bb7e63e4138221647f70cc36b
Isaac Hayward <Kodingroo@gmail.com>
2019-03-04 18:48:34 +0900

    user doc controller setup

---
+++ app/controllers/user_documents_controller.rb
@@ -0,0 +1,15 @@
+class UserDocumentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+end
