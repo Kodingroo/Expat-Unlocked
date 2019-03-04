c25e2eb9e66fa96e6ec5d3cf39d82697e7ebf511
sherly hartono <shartono1@gmail.com>
2019-03-04 17:20:42 +0900

    add column to user model, edit routes, edit users's edit.html.erb and new.html.erb, application controller, allow users to type username and address

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :user_documents, only: %i[index create show edit update]
   resources :documents, only: %i[index show create]
-
   # GET   /profile => profiles#show
-  # PATCH /profile => profiles#update
-  # GET   /profile/edit => profiles#edit
-  resource :profile, only: [ :show, :edit, :update ]
+  resource :profile, only: [ :show ]
 end
