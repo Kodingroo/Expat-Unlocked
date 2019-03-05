bf276f6f067bd32ae7509f93a59dc77b03d5564a
sherly hartono <shartono1@gmail.com>
2019-03-05 17:41:28 +0900

    seed venue model, comment it, cahnge profile controller name

--- app/controllers/profile_controller.rb
+++
@@ -1,7 +0,0 @@
-class ProfileController < ApplicationController
-  skip_after_action :verify_authorized
-
-  def show
-    # @cithyhall = current_user.near()
-  end
-end
--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,6 +7,7 @@ Geocoder.configure(
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
+   api_key: "pk.eyJ1Ijoic2hhcnRvbm8xIiwiYSI6ImNqcnZvejZsOTA0ZGI0YXBnNXdyMmFiNGQifQ.wMHuaZ_uzx8fBn3abvzl6g",
   # api_key: nil,               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
