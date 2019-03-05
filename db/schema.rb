a8d2b0bbe7281889354fa6b3b20018365f0128c2
Isaac <Hayward>
2019-03-05 13:57:34 +0900

    Admin added to the user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_024248) do
+ActiveRecord::Schema.define(version: 2019_03_05_045113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "documents", force: :cascade do |t|
-    t.string "jp_name"
+    t.string "doc_name"
     t.string "company_name"
     t.string "payment_type"
     t.string "description"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2019_03_05_024248) do
     t.string "last_name"
     t.string "address"
     t.string "photo"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
