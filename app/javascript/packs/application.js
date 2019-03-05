c685eb4c38be14c312284ca0b42879d210361f5d
Ivan <ivanvlora@gmail.com>
2019-03-05 23:02:00 +0900

    Implemented a animation and bg change when scrolling for the navbar. Added small hovers on user and document cards. Added logic for when a user has not name or picture on the user document page. Still need to fix the layout of the items in the navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,24 @@
-import "bootstrap";
+import 'bootstrap';
+
+const nav = document.querySelector('#navigation');
+
+0;
+
+const runOnScroll = () => {
+  if (document.body.scrollTop >= 200) {
+    nav.classList.add('navbar-top-fixed');
+  }
+};
+
+window.addEventListener('scroll', e => {
+  if (window.scrollY > 100) {
+    nav.style.background = '#000';
+    nav.style.color = '#fff';
+    nav.classList.add('sticky-nav');
+    nav.classList.remove('fadeout');
+  } else {
+    nav.classList.remove('sticky-nav');
+    nav.style.background = 'rgba(0,0,0, .4)';
+    nav.style.color = '#000';
+  }
+});
