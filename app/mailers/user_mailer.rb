7a93ff9129f4bf230b86a816641491af1478688d
sherly hartono <shartono1@gmail.com>
2019-03-06 18:42:18 +0900

    mailers test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,9 @@
 class UserMailer < ApplicationMailer
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
-  #
-  #   en.user_mailer.reminder.subject
-  #
-    def welcome(user)
-    @user = user  # Instance variable => available in view
+  # en.user_mailer.reminder.subject
+  def welcome(user)
+    @user = user # Instance variable => available in view
     @greeting = "Hi, hows it going mate?"
 
     Email.create!(name: user.first_name)
--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module ExpatUnlocked
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_mailer.delivery_method = :postmark
+    config.action_mailer.postmark_settings = { :api_token => "fa7bfc7b-1226-4bd3-ad76-d25350bd4319" }
   end
 end
