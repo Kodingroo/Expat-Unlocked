efa611fde52dd9f8f641d560cf92ca22d90a57e5
sherly hartono <shartono1@gmail.com>
2019-03-06 10:45:48 +0900

    master schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_064425) do
+ActiveRecord::Schema.define(version: 2019_03_05_045113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2019_03_05_064425) do
     t.string "title"
     t.string "doc_type"
     t.string "photo"
-    t.string "state"
+    t.boolean "state", default: false
     t.date "due_date"
     t.integer "remaining_balance"
     t.integer "current_due_amount"
@@ -64,20 +64,9 @@ ActiveRecord::Schema.define(version: 2019_03_05_064425) do
     t.string "last_name"
     t.string "address"
     t.string "photo"
-    t.float "latitude"
-    t.float "longitude"
     t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "venues", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
   end
 
   add_foreign_key "user_documents", "users"
