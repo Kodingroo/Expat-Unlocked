64f2b90aa3f28bfb7cb6ab31cbcb72f4cb8acfd5
Isaac <Hayward>
2019-03-06 15:55:44 +0900

    profile styling framework done

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,32 @@
 class ProfilesController < ApplicationController
+  before_action :set_user, only: [ :show ]
   skip_after_action :verify_authorized
 
   def show
+    # authorize @user
+    skip_authorization
+    @user_documents = UserDocument.all
   end
+
+  def update
+    authorize @user
+    @user.update(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+
+  def set_user
+    @user = current_user
+  end
+
 end
