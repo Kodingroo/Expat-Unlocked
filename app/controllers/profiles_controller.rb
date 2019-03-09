8124e9431421547e706c95db1980d38efdd73138
Isaac <Hayward>
2019-03-09 20:23:46 +0900

    Days Remaining = overdue logic complete

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,8 +9,9 @@ class ProfilesController < ApplicationController
     @user_document = UserDocument.new
     @documents = Document.all
 
+
     @sort_by = ["due date", "most expensive", "least expensive"]
-    @categories = ["all"]
+    @categories = []
     @collection_type = params[:sort_by]
 
     if @user_documents.exists?
@@ -18,16 +19,13 @@ class ProfilesController < ApplicationController
         @categories << doc.document.company_name
       end
 
+
       @categories.uniq!
       @collection_type = params[:sort_by]
 
-      unless params[:category] == "all"
-        @user_documents = @user_documents.reject { |doc| doc.document.company_name != params[:category] }
-      end
+      @user_documents = @user_documents.reject { |doc| doc.document.company_name != params[:category] }
 
-      @user_documents = if params[:category] == "all"
-        @user_documents.sort_by { |doc| doc.due_date }
-      elsif params[:sort_by] == "due date"
+      @user_documents = if params[:sort_by] == "due date"
         @user_documents.sort_by { |doc| doc.due_date }
       elsif params[:sort_by] == "most expensive"
         @user_documents.sort_by { |doc| -doc.current_due_amount }
