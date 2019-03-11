3d20e9dfb1f353c5ba4f41f09248e2e491f29b8f
sherly hartono <shartono1@gmail.com>
2019-03-11 17:29:19 +0900

    add user_document/edit route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       patch 'unpaid'
     end
   end
+  resources :user_documents, only: %i[update]
   # patch "user_documents/:id", to: "user_documents#update", as: :user_document_update
   resources :documents, only: %i[index show create]
   # GET   /profile => profiles#show
