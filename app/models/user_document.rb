1dae379b8af7b33b422a22904e507c6178972789
Ivan <ivanvlora@gmail.com>
2019-03-11 17:58:23 +0900

    user stats methods will be based on the current year

--- app/models/user_document.rb
+++ app/models/user_document.rb
@@ -7,18 +7,17 @@ class UserDocument < ApplicationRecord
   # FILTER = %w(due_date doc_type state)
   def self.most_expensive_bill(current_user)
     max_bill = current_user.user_documents.pluck("current_due_amount").max
-    current_user.user_documents.where(current_due_amount: max_bill)
-  end
-  
-  def self.least_expensive_bill(current_user)
-    min_bill = current_user.user_documents.pluck("current_due_amount").min
-    current_user.user_documents.where(current_due_amount: min_bill)
+    current_user.user_documents.where("created_at > ?", Date.new(Time.now.year, 1, 1)).where(current_due_amount: max_bill)
   end
 
-  def self.average_all_time(current_user)
-    # due_array = current_user.user_documents.pluck("current_due_amount")
-    # due_array.inject(0.0) { |sum, el| sum + el } / due_array.size
-    due_array = current_user.user_documents.pluck("current_due_amount")
+  def self.least_expensive_bill(current_user)
+    min_bill = current_user.user_documents.pluck("current_due_amount").min
+    current_user.user_documents.where("created_at > ?", Date.new(Time.now.year, 1, 1)).where(current_due_amount: min_bill)
+  end
+
+  def self.average_current_year(current_user)
+    current_user_docs = current_user.user_documents.where("created_at > ?", Date.new(Time.now.year, 1, 1))
+    due_array = current_user_docs.pluck("current_due_amount")
     due_array.inject(0.0) { |sum, el| sum + el } / due_array.size
   end
 
