037694577f925db5847e25a11d96ffdb6c9c5376
sherly hartono <shartono1@gmail.com>
2019-03-11 12:19:50 +0900

    remove gem base 64

--- app/models/user_document.rb
+++ app/models/user_document.rb
@@ -2,8 +2,6 @@ class UserDocument < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   belongs_to :user
   belongs_to :document, optional: true
-  mount_base64_uploader :photo, PhotoUploader
   # validates :photo, presence: true
   # validates :doc_type, presence: true
-  # FILTER = %w(due_date doc_type state)
 end
