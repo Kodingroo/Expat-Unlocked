000f1de7d81975a9cad4d1bf8ebc7667e8b4cec1
Isaac Hayward <kodingroo@gmail.com>
2019-03-12 11:58:08 +0900

    sign-up and sign-in styling finished

--- app/views/user_documents/index.js
+++
@@ -1,7 +0,0 @@
-console.log("wat");
-const dropdown_click = document.querySelector("#select-me");
-
-dropdown_click.addEventListener('click', (event) => {
-  // event.currentTarget.location.reload();
-  console.log(event);
-})
