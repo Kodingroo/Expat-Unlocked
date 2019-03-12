c0abcda4f6a57850f19df9985e6714aa0635d4bf
Ivan <ivanvlora@gmail.com>
2019-03-12 15:12:05 +0900

    added a javascript loader when adding an image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,22 @@
 //= require rails-ujs
 //= require_tree .
+function loader() {
+  const upload = document.querySelector('.upload-document');
+  console.log(upload.form);
+
+  // alert('submitting');
+  const loading = document.createElement('span');
+  const title = document.createElement('p');
+
+  title.innerText = 'Detecting Image';
+  title.style.fontWeight = 'bold';
+
+  loading.classList.add('pulse');
+
+  upload.form.insertAdjacentElement('beforeend', title);
+  upload.form.insertAdjacentElement('afterend', loading);
+
+  upload.form.submit();
+  upload.style.display = 'none';
+  upload.disabled = true;
+}
